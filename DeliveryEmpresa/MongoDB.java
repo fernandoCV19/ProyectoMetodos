13be069aeb9c8b142415ec145962fa58ebc30a61
Fernando Cuenca Vargas <fernandocuva@gmail.com>
2020-07-28 10:09:30 -0400

    Informes implementados

--- DeliveryEmpresa/MongoDB.java
+++ DeliveryEmpresa/MongoDB.java
@@ -31,24 +31,22 @@ public final class MongoDB
     public static String getInformeMasVendido(){
         MongoCollection collection_Productos = mongoDatabase.getCollection("Productos");
         MongoCollection collection_Pedidos = mongoDatabase.getCollection("Pedidos");
-        FindIterable<Document> productos = collection.find();
+        FindIterable<Document> productos = collection_Productos.find();
+        FindIterable<Document> pedidos = collection_Pedidos.find();
         HashMap<String, Long> informe = new HashMap<>();
-        String res = "Lista de productos y su cantidad de productos: ";
         long mayor = 0;
         String nombreMayor = "No existe";
+        String res= "LISTA DE PRODUCTOS Y LAS VECES QUE SE HAN VENDIDO\r";
         for(Document d: productos){
-            informe.put(d.get("name").toString(),new Long(0));
-        }
-        for(String s: informe.keySet()){
-            long act = collection_Pedidos.count(new Document("name",s));
-            informe.put(s, act);
+            long act = collection_Pedidos.count(d);
+            res = res + "Nombre del producto: " + d.get("name").toString();
+            res = res + "Cantidad de ventas: " + act + "\r";
             if(mayor<act){
                 mayor = act;
-                nombreMayor = s;
+                nombreMayor = d.get("name").toString();
             }
-            res = res + s + act + "\r";
         }
-        res = res + "Mas vendido: ";
+        res = res + "Mas vendido: " + nombreMayor+"\r";
         EscribeFichero.main(res, "RegistroProductos.txt");
         return res;
     }
@@ -57,7 +55,7 @@ public final class MongoDB
         MongoCollection collection_Pedidos = mongoDatabase.getCollection("Pedidos");
         FindIterable<Document> dealers = collection.find();
         HashMap<String, Long> informe = new HashMap<>();
-        String res = "Lista de productos y su cantidad de productos: ";
+        String res = "Lista de dealers y su cantidad de productos vendidos: \r";
         long mayor = 0;
         String nombreMayor = "No existe";
         for(Document d: dealers){
@@ -67,10 +65,10 @@ public final class MongoDB
                 mayor = act;
                 nombreMayor = d.get("name").toString();
             }
-            res = res +d.get("name").toString()+ act + "\r";
+            res = res + "Nombre: " +d.get("name").toString()+ act + "\r";
         }
-        res = res + "Mas vendido: ";
-        EscribeFichero.main(res, "RegistroProductos.txt");
+        res = res + "Mas vendido: "+nombreMayor;
+        EscribeFichero.main(res, "Mas vendedor.txt");
         return res;
     }
     public static double getGanancias(){
@@ -82,5 +80,78 @@ public final class MongoDB
             ganancia = ganancia + Integer.parseInt(doc.get("price").toString());
         }
         return ganancia;
+    }
+    public static String getVentas(){
+        MongoCollection collection_Pedidos = mongoDatabase.getCollection("Pedidos");
+        FindIterable<Document> productos = collection.find();
+        String res = "";
+        for(Document d: productos){
+            Document producto = (Document)d.get("producto");
+            Document usuario = (Document)d.get("usuario");
+            Document direccion = (Document)usuario.get("direccion");
+            res = res + d.getObjectId("_id").getDate().toString()+ "\r";
+            res = res + "PRODUCTO" + "\r";
+            res = res + "nombre: "+producto.get("name").toString()+ "\r";
+            res = res + "caracteristicas"+producto.get("features").toString()+ "\r";
+            res = res + "precio: "+producto.get("price").toString()+ "\r";
+            res = res + "USUARIO"+ "\r";
+            res = res + "nombre: "+usuario.get("name").toString()+ "\r";
+            res = res + "CI: "+usuario.get("CI").toString()+ "\r";
+            res = res + "celular: "+usuario.get("celular").toString()+ "\r";
+            res = res + "DIRECCION" + "\r";
+            res = res + "calle: "+direccion.get("calle").toString()+ "\r";
+            res = res + "nroCasa: "+direccion.get("nroCasa").toString()+ "\r";
+            res = res + "referencias: "+direccion.get("referencias").toString()+ "\r";
+            res = res + "CANTIDAD: "+direccion.get("cantidad").toString()+ "\r";
+            res = res + "OBSERVACIONES: "+direccion.get("observaciones").toString()+ "\r";
+        }
+        EscribeFichero.main(res, "Pedidos.txt");
+        return res;
+    }
+    public static String getInventario(){
+        MongoCollection collection_Pedidos = mongoDatabase.getCollection("Productos");
+        FindIterable<Document> productos = collection.find();
+        String res = "";
+        for(Document d: productos){
+            res = res + d.getObjectId("_id").getDate().toString()+ "\r";
+            res = res + "PRODUCTO" + "\r";
+            res = res + "nombre: "+d.get("name").toString()+ "\r";
+            res = res + "caracteristicas"+d.get("features").toString()+ "\r";
+            res = res + "precio: "+d.get("price").toString()+ "\r";
+        }
+        EscribeFichero.main(res, "Inventario.txt");
+        return res;
+    }
+    public static String getDealers(){
+        MongoCollection collection_Pedidos = mongoDatabase.getCollection("Dealers");
+        FindIterable<Document> dealers = collection.find();
+        String res = "";
+        for(Document d: dealers){
+            res = res + d.getObjectId("_id").getDate().toString()+ "\r";
+            res = res + "DEALER" + "\r";
+            res = res + "nombre: "+d.get("name").toString()+ "\r";
+            res = res + "codigo"+d.get("codigo").toString()+ "\r";
+        }
+        EscribeFichero.main(res, "Dealers.txt");
+        return res;
+    }
+    public static String getUsuarios(){
+        MongoCollection collection_Pedidos = mongoDatabase.getCollection("Usuarios");
+        FindIterable<Document> usuarios = collection.find();
+        String res = "";
+        for(Document usuario: usuarios){
+            Document direccion = (Document)usuario.get("direccion");
+            res = res + usuario.getObjectId("_id").getDate().toString()+ "\r";
+            res = res + "USUARIO"+ "\r";
+            res = res + "nombre: "+usuario.get("name").toString()+ "\r";
+            res = res + "CI: "+usuario.get("CI").toString()+ "\r";
+            res = res + "celular: "+usuario.get("celular").toString()+ "\r";
+            res = res + "DIRECCION" + "\r";
+            res = res + "calle: "+direccion.get("calle").toString()+ "\r";
+            res = res + "nroCasa: "+direccion.get("nroCasa").toString()+ "\r";
+            res = res + "referencias: "+direccion.get("referencias").toString()+ "\r";
+        }
+        EscribeFichero.main(res, "Usuarios.txt");
+        return res;
     }
 }
